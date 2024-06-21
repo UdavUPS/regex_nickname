693d7cec521a880df5183c50e9eadac89ffeb3c0
Udav-94 <ups-fti@yandex.ru>
2024-06-21 17:06:53 +0300

    Выполнено задание

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,21 @@
+import Validator from '../app';
+
+test('Validator', () => {
+    let obj = new Validator ('name');
+
+/*     let logs = ['Vasa', true, '1-qwe3', false];
+
+    for (let i = 0; i < logs.length; i+2) {
+
+        let result = obj.validateUsername(logs[i]);
+        expect(result).toBe(logs[i++]);
+        
+    } */
+
+        let result = obj.validateUsername('Vasa');
+        expect(result).toBe(true);
+
+        let result2 = obj.validateUsername('1-qwe3');
+        expect(result2).toBe(false);
+
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,17 @@
 // TODO: write your code here
-import sum from './basic';
+/* import sum from './basic';
 
 console.log('worked');
 
-console.log(sum([1, 2]));
+console.log(sum([1, 2])); */
+
+export default class Validator {
+    constructor(name) {
+        this.name = name;
+      }
+
+    validateUsername(name) {
+        return /^[^0-9 _-][a-z_0-9-]+[^0-9 _-]$/.test(name)
+        
+    }
+}
